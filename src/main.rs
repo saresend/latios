62953fbea83693332e4809b3a12209908d05b988
saresend <samresendez1@gmail.com>
2026-01-31 17:45:39 -0500

    setting up add workstream UI

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,7 @@ use ratatui::{
     buffer::Buffer,
     layout::Rect,
     style::Stylize,
-    symbols::border,
-    text::{Line, Text},
+    text::Line,
     widgets::{Block, Paragraph, Widget},
 };
 
@@ -56,10 +55,52 @@ impl Widget for &Workstream {
 }
 
 #[derive(Debug, PartialEq, Default)]
+pub struct NewWorkstream {
+    title: String,
+    spec_file: String,
+}
+
+impl NewWorkstream {
+    fn get_default_spec_location(&self) -> String {
+        format!("~/.latios/specs/{}.md", self.title)
+    }
+}
+
+impl Widget for &NewWorkstream {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Max(5), Constraint::Max(5)]);
+        let sections = layout.split(area);
+
+        let title_block = Block::bordered().title("Workstream Name");
+
+        self.title
+            .clone()
+            .render(title_block.inner(sections[0]), buf);
+        title_block.render(sections[0], buf);
+
+        let spec_location_block = Block::bordered().title("Spec file path");
+
+        let spec_file = if self.spec_file.is_empty() {
+            self.get_default_spec_location()
+        } else {
+            self.spec_file.clone()
+        };
+        spec_file.render(spec_location_block.inner(sections[1]), buf);
+        spec_location_block.render(sections[1], buf);
+    }
+}
+
+#[derive(Debug, PartialEq, Default)]
 pub struct LatiosApp {
     workstreams: Vec<Workstream>,
     exit: bool,
     curr_selected: usize,
+    is_creating_workstream: bool,
 }
 
 impl LatiosApp {
@@ -94,6 +135,15 @@ impl LatiosApp {
             std::cmp::max(0, std::cmp::min((self.workstreams.len() - 1) as isize, amt)) as usize;
         self.workstreams[self.curr_selected].highlight = true;
     }
+    pub fn open_new_workstream_view(&mut self) {
+        self.is_creating_workstream = true;
+    }
+
+    pub fn handle_escape(&mut self) {
+        if self.is_creating_workstream {
+            self.is_creating_workstream = false;
+        }
+    }
 
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
         while !self.exit {
@@ -113,6 +163,8 @@ impl LatiosApp {
                     KeyCode::Char('q') => self.exit = true,
                     KeyCode::Char('j') => self.change_selected(1),
                     KeyCode::Char('k') => self.change_selected(-1),
+                    KeyCode::Char('a') => self.open_new_workstream_view(),
+                    KeyCode::Esc => self.handle_escape(),
                     _ => {}
                 }
             }
@@ -132,25 +184,34 @@ impl Widget for &LatiosApp {
     where
         Self: Sized,
     {
+        let mut constraint_set = vec![Constraint::Max(3), Constraint::Min(40)];
+        if self.is_creating_workstream {
+            constraint_set.push(Constraint::Max(10));
+        }
+
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(constraint_set);
+
+        let sections = layout.split(area);
         let title = Span::styled(
             " Latios - Workstream management",
             Style::default().fg(Color::LightBlue).bg(Color::DarkGray),
         );
-        let layout = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(2)
-            .constraints([Constraint::Max(3), Constraint::Min(40)]);
 
-        let sections = layout.split(area);
         let workstream_layout = Layout::default()
             .direction(Direction::Vertical)
             .constraints(self.workstreams.iter().map(|x| Constraint::Max(3)));
         title.render(sections[0], buf);
         let stream_item = workstream_layout.split(sections[1]);
-        let title = Line::from("Workstreams: ".bold());
 
         for (i, workstream) in self.workstreams.iter().enumerate() {
             workstream.render(stream_item[i], buf);
+        }
+
+        if self.is_creating_workstream {
+            let new_workstream_widget = NewWorkstream::default();
+            new_workstream_widget.render(sections[2], buf);
         }
     }
 }
