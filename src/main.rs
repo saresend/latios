8d1740d3f18102dea40243620f13bec06ca3b533
saresend <samresendez1@gmail.com>
2026-01-31 12:21:33 -0500

    reorganizing for simpler workstream management

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,78 @@
-use crossterm::event::{Event, KeyCode};
 use ratatui::DefaultTerminal;
 use ratatui::prelude::*;
-use ratatui::widgets::FrameExt;
+use ratatui::widgets::*;
 use ratatui::widgets::{List, WidgetRef};
+use std::path::PathBuf;
 
-mod latios_types;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, read};
+use ratatui::{
+    Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
 
 use anyhow::Result;
-use latios_types::*;
 
-struct App {
-    curr_app_state: Box<dyn AppState>,
-    is_terminal: bool,
+fn main() -> Result<()> {
+    color_eyre::install();
+    let mut terminal = ratatui::init();
+    let mut app = LatiosApp::default();
+    app.run(&mut terminal)?;
+    ratatui::restore();
+    Ok(())
 }
 
-impl App {
-    fn run(&mut self, mut terminal: DefaultTerminal) -> Result<()> {
-        color_eyre::install().unwrap();
-        while !self.is_terminal {
-            terminal.draw(|frame| {
-                self.curr_app_state
-                    .render_ref(frame.area(), frame.buffer_mut())
-            });
-            let keypress = match crossterm::event::read()? {
-                Event::Key(key) => Some(key.code),
-                _ => None,
-            };
-            if let Some(keypress) = keypress {
-                self.consume_event(keypress);
-            }
+#[derive(Debug, PartialEq)]
+pub struct Workstream {
+    title: String,
+    needs_attention: bool,
+    spec_file: PathBuf,
+}
+
+#[derive(Debug, PartialEq, Default)]
+pub struct LatiosApp {
+    workstreams: Vec<Workstream>,
+    exit: bool,
+}
+
+impl LatiosApp {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
         }
         Ok(())
     }
 
-    fn consume_event(&mut self, keypress: KeyCode) {
-        match keypress {
-            KeyCode::Char('q') => self.is_terminal = true,
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+    fn handle_events(&mut self) -> Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                match key_event.code {
+                    KeyCode::Char('q') => self.exit = true,
+                    _ => {}
+                }
+            }
             _ => {}
         }
+        Ok(())
     }
 }
 
-impl WidgetRef for App {
-    fn render_ref(&self, area: Rect, buf: &mut Buffer) {
-        self.curr_app_state.render_ref(area, buf);
+impl Widget for &LatiosApp {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        let title = Line::from(" Latios - Agentic Workstream management".bold());
+        let block = Block::bordered().title(title.centered());
+
+        block.render(area, buf);
     }
-}
-
-struct Home {
-    workstreams: Vec<Workstream>,
-    curr_workstream: usize,
-}
-
-impl WidgetRef for Home {
-    fn render_ref(&self, area: Rect, buf: &mut Buffer) {
-        let layout = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([Constraint::Length(10), Constraint::Min(50)])
-            .split(area);
-
-        "Current Workstreams".render(layout[0], buf);
-        let list = List::new(self.workstreams.iter().map(|wstream| wstream.get_line()));
-        Widget::render(list, layout[1], buf);
-    }
-}
-impl AppState for Home {}
-
-fn main() {
-    let sample_workstreams = vec![
-        Workstream::new("Invoke claude", WorkstreamType::Claude),
-        Workstream::new("Invoke Codex", WorkstreamType::Codex),
-    ];
-
-    let terminal = ratatui::init();
-    let mut app = App {
-        curr_app_state: Box::new(Home {
-            workstreams: sample_workstreams,
-            curr_workstream: 0,
-        }),
-        is_terminal: false,
-    };
-    let result = app.run(terminal);
-    ratatui::restore();
 }
